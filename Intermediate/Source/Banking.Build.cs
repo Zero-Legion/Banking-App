5507d7191e7aeffa8a6967267c3a7c7aa974124e
Dragos <tadragos@yahoo.com>
2021-10-28 19:28:15 +0300

    2FA v1.1 Update
    2FA v1.1 Update

---
+++ Intermediate/Source/Banking.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class Banking : ModuleRules
+{
+	public Banking(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
